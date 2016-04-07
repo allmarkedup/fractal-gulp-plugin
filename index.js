f135ff66936c3e1964bf5588286c3bfd5c643c77
Mark Perkins <mark@allmarkedup.com>
2016-04-07 12:02:19 +0100

    Add better logging and error handling

--- index.js
+++ index.js
@@ -2,50 +2,54 @@
 'use strict';
 
 const path        = require('path');
+const ptime       = require('pretty-hrtime');
+const chalk       = require('chalk');
 const packageJSON = require('./package.json');
 
 module.exports = function (config, app) {
 
     this.name = 'gulp-plugin';
-    this.title = 'app Gulp tasks plugin';
+    this.title = 'Fractal Gulp tasks plugin';
     this.version = packageJSON.version;
 
-    const console = app.console;
-    let gulp;
-
     if (!config.gulp) {
-        const gulpFile = require(path.join(process.cwd(), 'gulpfile.js'));
-        gulp = require('gulp');
-    } else {
-        gulp = config.gulp;
+        throw new Error('You must pass a Gulp instance as part of the configuration');
     }
 
-    logEvents(gulp);
-
-    this.command('gulp [task]', {
+    const console = app.console;
+    const gulp = config.gulp;
+    const opts = {
         description: 'Run the specified Gulp task.'
-    }, (args, done) => {
+    };
+
+    gulp.on('task_start',     e => console.notice(`Starting ${chalk.cyan(e.task)} task`));
+    gulp.on('task_stop',      e => console.notice(`Finished ${chalk.cyan(e.task)} in ${chalk.magenta(ptime(e.hrDuration))}`));
+    gulp.on('task_err',       e => console.error(`Task '${chalk.bold(e.task)}' errored after ${ptime(e.hrDuration)}.`));
+    gulp.on('task_not_found', e => console.error(`Task ${chalk.bold(e.task)} not found`));
+
+    this.command('gulp [task]', opts, (args, done) => {
         const task = args.task || 'default';
         console.log('-----');
-        gulp.start(task, err => {
+        gulp.start(task, e => {
+            if (e) {
+                console.error(`→ ${formatError(e)}`);
+            }
             console.log('-----');
             done();
         });
     });
 
-    function logEvents(g) {
-        g.on('task_start', function(e) {
-            console.notice(`Starting ${e.task}...`);
-        });
-        g.on('task_stop', function(e) {
-            console.notice(`Finished ${e.task}`);
-        });
-        g.on('task_error', function(e) {
-            console.error(`There was a problem with the Gulp '${task}' task.`);
-        });
-        g.on('task_not_found', function(err) {
-            console.error(`Task ${err.task} not found`)
-        });
+    function formatError(e) {
+        if (!e.err) {
+            return e.message;
+        }
+        if (typeof e.err.showStack === 'boolean') {
+            return e.err.toString();
+        }
+        if (e.err.stack) {
+            return e.err.stack;
+        }
+        return new Error(String(e.err)).stack;
     }
 
 };
