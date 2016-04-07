6e450276ca27471887511e44612b9b3fb4a04e98
Mark Perkins <mark@allmarkedup.com>
2016-04-07 10:05:52 +0100

    Get basic functionality working

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # Gulp tasks plugin for Fractal.
 
-[WIP]
+A helper command to run your Gulp tasks from within Fractal's interactive CLI.
+
+## Installation
+
+Install via NPM:
+
+```shell
+npm i @allmarkedup/fractal-gulp-plugin --save
+```
+
+In your `gulpfile.js`, export the gulp instance that you have added your tasks to. For example:
+
+```js
+// gulpfile.js
+'use strict';
+
+const gulp = require('gulp');
+const del  = require('del');
+
+gulp.task('clean', function() {
+    return del(['./public','./another/directory']);
+});
+
+module.exports = gulp; // Export the gulp instance
+```
+
+Then configure the plugin in your `fractal.js` file:
+
+```js
+// fractal.js
+'use strict';
+
+const fractal = require('@frctl/fractal');
+
+fractal.plugin('@allmarkedup/fractal-gulp-plugin', {
+    gulp: require('./gulpfile')
+});
+```
+
+## Usage
+
+This plugin exposes a single `gulp` command when you are using the [Fractal interactive CLI](https://github.com/frctl/fractal/blob/master/docs/commands.md#the-fractal-interactive-cli-sparkles).
+
+Any gulp task in your `gulpfile.js` can then be run using the familiar `gulp [task]` format, without having to leave the Fractal CLI.
--- index.js
+++ index.js
@@ -7,44 +7,44 @@ const packageJSON = require('./package.json');
 module.exports = function (config, app) {
 
     this.name = 'gulp-plugin';
-    this.title = 'Fractal Gulp tasks plugin';
+    this.title = 'app Gulp tasks plugin';
     this.version = packageJSON.version;
 
     const console = app.console;
     let gulp;
 
     if (!config.gulp) {
-        const gulpFile = require(path.join(process.cwd, 'gulpfile.js'));
+        const gulpFile = require(path.join(process.cwd(), 'gulpfile.js'));
         gulp = require('gulp');
     } else {
         gulp = config.gulp;
     }
 
     logEvents(gulp);
-    
+
     this.command('gulp [task]', {
         description: 'Run the specified Gulp task.'
     }, (args, done) => {
         const task = args.task || 'default';
         console.log('-----');
         gulp.start(task, err => {
-            this.console.log('-----');
+            console.log('-----');
             done();
         });
     });
 
     function logEvents(g) {
         g.on('task_start', function(e) {
-            fractal.console.notice(`Starting ${e.task}...`);
+            console.notice(`Starting ${e.task}...`);
         });
         g.on('task_stop', function(e) {
-            fractal.console.notice(`Finished ${e.task}`);
+            console.notice(`Finished ${e.task}`);
         });
         g.on('task_error', function(e) {
-            fractal.console.error(`There was a problem with the Gulp '${task}' task.`);
+            console.error(`There was a problem with the Gulp '${task}' task.`);
         });
         g.on('task_not_found', function(err) {
-            fractal.console.error(`Task ${err.task} not found`)
+            console.error(`Task ${err.task} not found`)
         });
     }
 
